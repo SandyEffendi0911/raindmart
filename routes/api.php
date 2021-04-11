7649dd5f4a7fa38b30165d084a981efb6c028c5d
anisfikriyyah <anisfikri3@gmail.com>
2021-04-11 12:31:43 +0700

    Add passport, cors and jsonResponse

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+
+    // public routes
+    Route::post('/login', 'API\AuthController@login');
+    Route::post('/register','API\AuthController@register');
+    Route::post('/logout', 'API\AuthController@logout');
+
+});
